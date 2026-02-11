fe3cd79a0a4ee99b3534f9e25bee8456c92410cf
Sanjana6200 <reddemsanjana@gmail.com>
2026-02-12 00:17:43 +0530

    Add files via upload

---
+++ Teachers.java
@@ -0,0 +1,33 @@
+class Teachers {
+    public static void main(String[] args) {
+        
+        
+        byte smallNumber = 10;
+        short mediumNumber = 1000;
+        int teacherID = 202;
+        long bigNumber = 100000L;
+
+        float averageMarks = 85.5f;
+        double totalMarks = 100.0;
+
+        
+        char rating = 'A';
+
+        
+        boolean isGood = true;
+
+        
+        String teacherName = "Suthir Sir";
+
+        
+        System.out.println("Small Number (byte): " + smallNumber);
+        System.out.println("Medium Number (short): " + mediumNumber);
+        System.out.println("Teacher ID (int): " + teacherID);
+        System.out.println("Big Number (long): " + bigNumber);
+        System.out.println("Average Marks (float): " + averageMarks);
+        System.out.println("Total Marks (double): " + totalMarks);
+        System.out.println("Rating (char): " + rating);
+        System.out.println("Is Good? (boolean): " + isGood);
+        System.out.println("Teacher Name (String): " + teacherName);
+    }
+}
